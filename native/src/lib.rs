2b7647452c47c7533b3c97a368f1efd441cf435d
luddite478 <37404269+luddite478@users.noreply.github.com>
2020-01-16 22:43:29 +0300

    spacces fix

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -6,27 +6,27 @@ extern crate base64;
 use neon::prelude::*;
 
 fn raw_to_jpeg(mut cx: FunctionContext) -> JsResult<JsString> {
-	let base64_raw_img = cx.argument::<JsString>(0)?.value();
-	let imgx = cx.argument::<JsNumber>(1)?.value();
-	let imgy = cx.argument::<JsNumber>(2)?.value();
+  let base64_raw_img = cx.argument::<JsString>(0)?.value();
+  let imgx = cx.argument::<JsNumber>(1)?.value();
+  let imgy = cx.argument::<JsNumber>(2)?.value();
 
-    let raw_img_vec = base64::decode(&base64_raw_img).unwrap();
+  let raw_img_vec = base64::decode(&base64_raw_img).unwrap();
 
-    let dyn_image = image::load_from_memory(&raw_img_vec[..]).unwrap();
-    let raw_img = dyn_image.into_rgb().into_raw();
+  let dyn_image = image::load_from_memory(&raw_img_vec[..]).unwrap();
+  let raw_img = dyn_image.into_rgb().into_raw();
 
-    let mut jpg_vec: Vec<u8> = Vec::new();
-    let quality = 0; // 0 - worst, 100 - best
-    let mut encoder = image::jpeg::JPEGEncoder::new_with_quality(&mut jpg_vec, quality);
+  let mut jpg_vec: Vec<u8> = Vec::new();
+  let quality = 0; // 0 - worst, 100 - best
+  let mut encoder = image::jpeg::JPEGEncoder::new_with_quality(&mut jpg_vec, quality);
 
-	encoder.encode(&raw_img, imgx as u32, imgy as u32, image::ColorType::RGB(8)); 
-	let base64_jpg = base64::encode(&jpg_vec[..]);
+  encoder.encode(&raw_img, imgx as u32, imgy as u32, image::ColorType::RGB(8)); 
+  let base64_jpg = base64::encode(&jpg_vec[..]);
 
-	Ok(cx.string(base64_jpg))
+  Ok(cx.string(base64_jpg))
 }
 
 register_module!(mut m, {
-    m.export_function("rawToJpeg", raw_to_jpeg)
+  m.export_function("rawToJpeg", raw_to_jpeg)
 });
 
 
